57b258229546fd6851a248ba6f32d3188b42386e
sasethi <sahil.sethi377@gmail.com>
2026-06-07 16:47:22 +0530

    Initial Commit

--- src/main/java/org/acme/MyMessagingApplication.java
+++ src/main/java/org/acme/MyMessagingApplication.java
@@ -1,47 +1,21 @@
 package org.acme;
 
-import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.event.Observes;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.apache.camel.builder.RouteBuilder;
+
 
 import java.util.stream.Stream;
 
 @ApplicationScoped
-public class MyMessagingApplication {
+public class MyMessagingApplication extends RouteBuilder  {
 
-    /**
-     * Injects an emitter to send messages to the "words-out" channel.
-     */
-    @Channel("words-out")
-    Emitter<String> emitter;
 
-    /**
-     * Sends message to the "words-out" channel, can be used from a JAX-RS resource or any bean of your application.
-     * Messages are sent to the broker.
-     **/
-    void onStart(@Observes StartupEvent ev) {
-        Stream.of("Hello", "with", "Quarkus", "Messaging", "message").forEach(string -> emitter.send(string));
-    }
+    @Override
+    public void configure() throws Exception {
 
-    /**
-     * Consume the message from the "words-in" channel, uppercase it and send it to the uppercase channel.
-     * This method is called by the framework when a message is received on the "words-in" channel (from the broker).
-     **/
-    @Incoming("words-in")
-    @Outgoing("uppercase")
-    public String toUpperCase(String message) {
-        return message.toUpperCase();
-    }
 
-    /**
-     * Consume the uppercase channel (coming from within the application) and print the messages to the console.
-     **/
-    @Incoming("uppercase")
-    public void sink(String word) {
-        System.out.println(">> " + word);
+        from("timer:foo?period=10000")
+                .setBody(constant("Hello from Camel Quarkus!"))
+                .to("log:example-logger");
     }
 }
